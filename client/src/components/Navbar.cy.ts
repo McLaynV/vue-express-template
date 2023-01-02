65cd104fa18dbfeda737442cb3247e1e56001b20
McLayn <mclayn.v@gmail.com>
2023-01-02 21:38:38 +0100

    refactor test

--- client/src/components/Navbar.cy.ts
+++ client/src/components/Navbar.cy.ts
@@ -5,8 +5,11 @@ describe("<Navbar />", () => {
     // see: https://test-utils.vuejs.org/guide/
     cy.mount(Navbar);
     cy.get("h1").should("contain.text", "Vue App");
-    cy.get("a.burger").should("not.have.class", "is-active");
-    cy.get("a.burger").click().should("have.class", "is-active");
-    cy.get("a.burger").click().should("not.have.class", "is-active");
+    cy.get("a.burger")
+      .should("not.have.class", "is-active")
+      .click()
+      .should("have.class", "is-active")
+      .click()
+      .should("not.have.class", "is-active");
   });
 });
